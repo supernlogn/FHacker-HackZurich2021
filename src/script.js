b4a59eca0dc8e75076cf0eaa0080db8d828d36a0
MikeDoes <40600201+MikeDoes@users.noreply.github.com>
2021-09-25 17:04:18 +0200

    Added double clicjk event

--- src/script.js
+++ src/script.js
@@ -15,3 +15,9 @@ function getSelectionText(){
     
     console.log(output_text)
 }
+
+var article = document.getElementById('article')
+
+article.addEventListener('dblclick', function(){
+    console.log(getSelectionText())
+})
