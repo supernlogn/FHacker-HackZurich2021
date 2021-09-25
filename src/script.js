573ae50c3bb52c32a416c804e13f413d616895f9
MikeDoes <40600201+MikeDoes@users.noreply.github.com>
2021-09-25 18:05:29 +0200

    added listrners

--- src/script.js
+++ src/script.js
@@ -1,3 +1,53 @@
+var onlongtouch; 
+var timer;
+var touchduration = 800; //length of time we want the user to touch before we do something
+
+var sentence_text = ''
+
+window.addEventListener("contextmenu", e => e.preventDefault());
+window.addEventListener("selectionchange", e => e.preventDefault());
+
+function touchstart(e) {
+    sentence_text = e.target.innerHTML
+    e.preventDefault();
+    if (!timer) {
+        timer = setTimeout(onlongtouch, touchduration);
+    }
+}
+
+function touchend() {
+    //stops short touches from firing the event
+    if (timer) {
+        clearTimeout(timer);
+        timer = null;
+    }
+}
+
+onlongtouch = function() { 
+    timer = null;
+    alert(sentence_text)
+};
+
+
+
+
+document.addEventListener("DOMContentLoaded", function(event) {
+    
+
+    var sentences = document.getElementsByClassName('sentence')
+    
+    for (let i = 0; i < sentences.length; i++) {
+        sentences[i].addEventListener("touchstart", touchstart, false);
+        sentences[i].addEventListener("touchend", touchend, false);
+      }
+
+});
+
+
+
+
+
+/* 
 function getSelectionText(){
 
     var text = "";
@@ -6,7 +56,7 @@ function getSelectionText(){
     } else if (document.selection && document.selection.type != "Control") {
         text = document.selection.createRange().text;
     }
-    /* If nothing is highlighted, do nothing */
+     If nothing is highlighted, do nothing 
 
     let output_text = ''+document.getSelection().toString()
 
@@ -21,4 +71,4 @@ var article = document.getElementById('article')
 article.addEventListener('dblclick', function(){
     console.log('event click works')
     console.log(getSelectionText())
-})
+}) */
