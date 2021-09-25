0b17f2e4bd5bac39c47832bbf4a5d99085e97d45
Ioannis Athanasiadis <ath.ioannis94@gmail.com>
2021-09-25 21:25:32 +0200

    http request

--- src/codeForPopups.js
+++ src/codeForPopups.js
@@ -1,5 +1,6 @@
-SERVER  = ""
-APP_PATH = ""
+SERVER  = "inference.hackzurich2021.hack-with-admin.ch"
+APP_PATH = "api/question/hack_zurich"
+
 var lastToolTipChosen = null;
 function createToolTip(el, text) {
    lastToolTipChosen = el;
@@ -10,16 +11,52 @@ function createToolTip(el, text) {
    // newDiv.style.top = y + "px";
 }
 
+function getInfoForQuestion(questionText)
+{   let responseText = {};
+    let xmlHttp = new XMLHttpRequest();
+    xmlHttp.onreadystatechange = function() {
+        if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
+            responseText = xmlHttp.response;
+        }
+        else {
+            console.log("status " + xmlHttp.status);
+        }
+    }
+    httpPath = SERVER + "/" + APP_PATH;
+    xmlHttp.open("PUT", httpPath, true); // true for asynchronous
+    xmlHttp.setRequestHeader("Content-Type", "application/json;charset=utf-8");
+    xmlHttp.setRequestHeader("X-API-Key", "sjNmaCtviYzXWlS");
+    // xmlHttp.setRequestHeader("Connection", "keep-alive");
+    xmlHttp.send({"question": questionText, "beam_size": 2});
+    console.log("request to " + httpPath);
+    return responseText;
+}
+
+// function httpPOSTAsync(whereDoYouWantToPost, dataToPost)
+// {   let responseText ="";
+//     var xmlHttp = new XMLHttpRequest();
+//     xmlHttp.onreadystatechange = function() {
+//         if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
+//             responseText = xmlHttp.responseText;
+//         }
+//         else {
+//             console.log("status " + xmlHttp.status);
+//         }
+//     }
+//     xmlHttp.open("POST", SERVER+"/"+APP_PATH +"/" + whereDoYouWantToPost, true); // true for asynchronous 
+//     xhttp.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+//     xmlHttp.send(dataToPost);
+//     return responseText;
+// }
+
+
 function hideToolTip() {
   lastToolTipChosen.removeAttribute("data-tooltip");
 }
 
-function getAcccuracyForText() {
-    navigator.sendBeacon(SERVER + "/" + APP_PATH);
-}
 
 function requestQuestionSRF() {
     let questionText = "";
-    let 
+    navigator. 
     return 
 }
